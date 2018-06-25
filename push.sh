444ed45c5f02574a617348f4e73f26e8593c0d11
Sourcemodding <root@squixl.com>
2018-06-25 10:05:05 +0000

    fix

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 
 cd /home/web/sourcemodding/wp-content/tables/
 git add .
-commit -m "fix"
-push https://sourcemodding:Valve4Life@github.com/sourcemodding/Sourcemodding-Tablepress-Table-Sources master
+git commit -m "fix"
+git push https://sourcemodding:Valve4Life@github.com/sourcemodding/Sourcemodding-Tablepress-Table-Sources master
 
