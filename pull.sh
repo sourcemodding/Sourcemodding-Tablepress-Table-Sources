901bce489d497b74745fb7eb076f9e4244af1bfe
Sourcemodding <root@squixl.com>
2018-06-29 21:14:55 +0000

    fix

--- pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/web/sourcemodding/wp-content/tables/
-git pull origin master
-chown -R www-data:www-data .
--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/web/sourcemodding/wp-content/tables/
-git add .
-git commit -m "fix"
-git push https://sourcemodding:Valve4Life@github.com/sourcemodding/Sourcemodding-Tablepress-Table-Sources master
-
